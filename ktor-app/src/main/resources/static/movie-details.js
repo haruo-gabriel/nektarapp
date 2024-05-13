16d8025f143be06414ec52a3001319e151a104af
haruo <takeuchigabriel@usp.br>
2024-05-13 17:49:24 -0300

    Complementando frontend.

--- ktor-app/src/main/resources/static/movie-details.js
+++ ktor-app/src/main/resources/static/movie-details.js
@@ -7,31 +7,6 @@ window.onload = function() {
     populateMovieDetails(currentMovie).catch(error => console.error(error));
 }
 
-/*
-async function populateMovieDetails(movie) {
-    // Fetch the HTML template
-    const templateResponse = await fetch('./movie-details-template.html');
-    let html = await templateResponse.text();
-
-    // Create a Date object from the release_date string
-    const releaseDate = new Date(movie.release_date);
-    // Format the release date as dd/mm/yyyy
-    const formattedReleaseDate = releaseDate.getDate() + '/' + (releaseDate.getMonth() + 1) + '/' + releaseDate.getFullYear();
-
-    // Replace all occurrences of the placeholders with the actual movie details
-    html = html.replace(/{title}/g, movie.title)
-        .replace(/{backdrop_path}/g, imagesBaseUrl + '/original' + movie.backdrop_path)
-        .replace(/{overview}/g, movie.overview)
-        .replace(/{poster_path}/g, imagesBaseUrl + '/w300' + movie.poster_path)
-        .replace(/{release_date}/g, formattedReleaseDate)
-        .replace(/{vote_average}/g, movie.vote_average);
-
-    // Insert the new HTML into a specific container in the index.html page
-    const container = document.querySelector('.movie-details.all-container');
-    container.innerHTML = html;
-}
-*/
-
 async function populateMovieDetails(movie) {
     // Create a Date object from the release_date string
     const releaseDate = new Date(movie.release_date);
