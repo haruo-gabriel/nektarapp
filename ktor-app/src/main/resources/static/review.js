142821ecabe2fcabb202482f16756f3d680f1c4b
haruo <takeuchigabriel@usp.br>
2024-05-28 11:02:56 -0300

    Corrigindo o HTML do gerador de componentes.

--- ktor-app/src/main/resources/static/review.js
+++ ktor-app/src/main/resources/static/review.js
@@ -5,8 +5,8 @@ function generateReviewHTML(username, creationDate, reviewText) {
     // Create the HTML template for the review
     const reviewHTML = `
         <div class="review">
-            <p class="review-title">${username} em ${formattedDate}</p>
-            <p>${reviewText}</p>
+            <b class="review-title">${username} em ${formattedDate}</b>
+            <p class="review-text">${reviewText}</p>
         </div>
     `;
 
